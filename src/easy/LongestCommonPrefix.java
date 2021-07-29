a0d347bbbf9acb18d50cdee26b12d3b242dd64fe
Sergey Rakov <rakovsergey94@gmail.com>
2021-07-29 23:33:10 +0300

    .

--- src/easy/LongestCommonPrefix.java
+++ src/easy/LongestCommonPrefix.java
@@ -2,24 +2,27 @@ package easy;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] array = {"flow", "flower", "flight"};
+        String[] array = {"flower", "flow", "flight"};
         System.out.println(longestCommonPrefix(array));
     }
 
 //    Input: strs = ["flower","flow","flight"]
 //    Output: "fl"
     public static String longestCommonPrefix(String[] strs) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("");
         int index = 0;
         int length = strs[0].length();
 
-        while (index <= length) {
+        while (index < length) {
             for (int i = 0; i < strs.length - 1; i++) {
-                if (strs[i].charAt(index) != (strs[i + 1].charAt(index))) {
+                if (Math.min(index, strs[i + 1].length() - 1) < index) {
+                    return result.toString();
+                }
+                if (strs[i].charAt(index) != (strs[i + 1].charAt(Math.min(index, strs[i + 1].length() - 1)))) {
                     return result.toString();
                 }
             }
-            result.append(strs[index].charAt(index));
+            result.append(strs[0].charAt(index));
             index++;
         }
         return result.toString();
